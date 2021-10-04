74fe764bf47855c9388272b8e16342232554088c
saifulislammunna <saifulmunna28@gmail.com>
2021-10-04 18:32:44 +0600

    style-added

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -3,11 +3,14 @@ import './Service.css'
 const Service = (props) => {
     const {img,name,price} = props.language
     return (
-        <div className="language-course">
+       
+        <div className="language-course  ">
             <img src={img} className="rounded" alt="" width="400" height="300" />
             <h1>Name:{name}</h1>
             <p>Price:{price}</p>
+            
         </div>
+         
     );
 };
 
