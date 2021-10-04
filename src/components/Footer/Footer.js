b9e9a2e1d6415d9783868d79f4c97f4e5489a55d
saifulislammunna <saifulmunna28@gmail.com>
2021-10-04 22:13:09 +0600

    style-added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,15 +2,10 @@ import React from 'react';
 import './Footer.css'
 /* footer component */
 const Footer = () => {
-    var footerStyle = {
-        display: 'block',
-        padding: '20px',
-        height: '60px',
-        width: '100%',
-      }
+  
     return (
       <div >
-                <div   style={{footerStyle}}/> 
+                <div  /> 
         <div className="footer ">
             <p>COPYRIGHT © 2021 LC</p>
         </div>
